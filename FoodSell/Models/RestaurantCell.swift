f60dc59a23d0798abf449c13c113a5f88af26b1e
user132893 <user132893@74-80-239-250.ic2net.net>
2018-04-09 16:11:50 -0700

    fixing master

---
+++ FoodSell/Models/RestaurantCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantCell.swift
+//  FoodSell
+//
+//  Created by angel gonzalez on 4/2/18.
+//  Copyright © 2018 user132893. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
